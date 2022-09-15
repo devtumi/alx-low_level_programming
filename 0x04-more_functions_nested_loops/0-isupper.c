e159e55681321d4229ed3623992c3403bf9b06d3
Tumininu Akande <tumiakande@gmail.com>
2022-09-15 16:59:39 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * @c: input character
+ *
+ * Return: returns1 if uppercase
+ * otherwise returns 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
